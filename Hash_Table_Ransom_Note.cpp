6f09461a877d71538a8a3e163ceb7b864f37982f
Vincent K <32659560+vkri801@users.noreply.github.com>
2021-05-06 17:17:20 +1200

    Create Hash_Table_Ransom_Note.cpp

---
+++ Hash_Table_Ransom_Note.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the checkMagazine function below.
+void checkMagazine(vector<string> magazine, vector<string> note) {
+    unordered_map<string, int> words;
+    for(auto const &it:magazine){
+        //fill up hashmap with string as key and number of occurences as value
+        words[it]++;
+    }
+    for(auto const &it:note){
+        //default value is 0
+        //if it was filled in previous for loop, it will have value of >1
+        if(words[it]>0){
+            //decrement value if found 
+            words[it]--;
+        }
+        else{
+            //if not originally found in magazine, break and print out No
+            cout<< "No";
+            return;
+        }
+    }
+    //if all words were found, we will proceed to here
+    cout<<"Yes";
+    return;
+
+}
+
+int main()
+{
+    //initialise and acquire num of words in magazine and note
+    string mn_temp;
+    getline(cin, mn_temp);
+
+    vector<string> mn = split_string(mn_temp);
+
+    int m = stoi(mn[0]);
+
+    int n = stoi(mn[1]);
+
+    string magazine_temp_temp;
+    getline(cin, magazine_temp_temp);
+
+    vector<string> magazine_temp = split_string(magazine_temp_temp);
+
+    vector<string> magazine(m);
+
+    for (int i = 0; i < m; i++) {
+        string magazine_item = magazine_temp[i];
+
+        magazine[i] = magazine_item;
+    }
+
+    string note_temp_temp;
+    getline(cin, note_temp_temp);
+
+    vector<string> note_temp = split_string(note_temp_temp);
+
+    vector<string> note(n);
+
+    for (int i = 0; i < n; i++) {
+        string note_item = note_temp[i];
+
+        note[i] = note_item;
+    }
+
+    checkMagazine(magazine, note);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
